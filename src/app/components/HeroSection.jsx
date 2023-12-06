2422fd9eb426a4c557322ff059c61f602bff07fd
Abhinav Jain <94744519+Abhinav-Jain252@users.noreply.github.com>
2023-12-07 00:52:01 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
             <motion.div initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration: 0.5}} className='col-span-4 place-self-center mt-4 lg:mt-0'>
                 <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative'>
                     <Image 
-                        src="/images/hero-image.png"
+                        src="/images/hero-image2.png"
                         alt="hero image"
                         className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
                         width={300}
