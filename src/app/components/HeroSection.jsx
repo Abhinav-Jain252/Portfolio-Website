a03e29591c864e94473fd2c4ccb414a25ce61714
Abhinav Jain <94744519+Abhinav-Jain252@users.noreply.github.com>
2023-12-07 00:52:34 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,8 +40,8 @@ const HeroSection = () => {
                         src="/images/hero-image2.png"
                         alt="hero image"
                         className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
-                        width={300}
-                        height={300}
+                        width={600}
+                        height={600}
                     />
                 </div>
             </motion.div>
